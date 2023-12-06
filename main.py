ed09acf84953fbecec4a71a033e5e008947daa04
Sol Fernandez Arancibia <solfernandezarancibia@gmail.com>
2023-12-06 13:16:24 -0300

    add function to extract datapoints from table and corresponding test

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Datapoint:
-    datetime: datetime
+    date_time: datetime
     location: str
     height: float
 
@@ -15,56 +15,35 @@ class Datapoint:
 with open('tabla.html') as f:
     html = f.read()
 
-# parse html and extract table
-soup = BeautifulSoup(html, features = 'html.parser')
-table = soup.find_all("table")[0]
 
-# extract table rows
+def datapoints_from_table(html: str) -> set[Datapoint]:
+    # parse html and extract table
+    soup = BeautifulSoup(html, features='html.parser')
+    table = soup.find_all("table")[0]
 
-rows = BeautifulSoup(str(table), features = 'html.parser').find_all("tr")
+    # extract table rows
+    rows = BeautifulSoup(str(table), features='html.parser').find_all("tr")
 
-'''
-tablesoup = BeautifulSoup(str(table), features = 'html.parser')
-# Extraer todas las filas
-rows = []
-for elem in tablesoup.find_all("tr"):
-    rows.append(elem)
-'''
+    # extract date and time of last measurement from first row
+    date, time = BeautifulSoup(str(rows[0]), 'html.parser').find_all("th")[2].text.strip().split()
+    date_time = datetime.strptime(date + ' ' + time, '%d/%m/%Y %H:%M')
 
-# extract date and time of last measurement from first row
+    # extract location and tide height from following rows, create datapoints
 
-date, time = BeautifulSoup(str(rows[0]), 'html.parser').find_all("th")[2].text.strip().split()
-print( 'datetime:', date, time)
-
-'''
-# Extraer elementos de la primera fila
-horarios = []
-for el in BeautifulSoup(str(rows[0]), 'html.parser').find_all("th"):
-    horarios.append(el)
-
-# guardar datos de ultima medicion
-fecha, hora = horarios[2].text.strip().split()
-print('datetime:', fecha, hora)
-'''
+    datapoints = set()
+    for row in rows[1:]:
+        els = BeautifulSoup(str(row), 'html.parser').find_all("td")
+        location = els[1].text.strip().replace('\n', '')
+        height_str = els[2].text.strip().replace(',', '.')
+        try:
+            height = float(height_str)
+        except ValueError:
+            height = None
+        datapoints.add(Datapoint(date_time=date_time, location=location, height=height))
+    return datapoints
 
 
-
-locations = []
-heights = []
-for row in rows[1:]:
-    els = BeautifulSoup(str(row), 'html.parser').find_all("td")
-    locations.append(els[1].text.strip())
-    heights.append(els[2].text.strip())
-print('locations:', locations)
-print('heights:', heights)
-
-
-
-# Locations
-#locations = []
-#for element in tablesoup.find_all("span"):
-#    locations.append(element.string)
-#print(locations)
-
-
+datapoints = datapoints_from_table(html)
+for datapoint in datapoints:
+    print(datapoint)
 
